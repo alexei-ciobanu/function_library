8906bbb54819ebfd82089ef031190c74830a34f1
Alexei Ciobanu <alexei.ciobanu@auspost.com.au>
2023-08-30 17:30:28 +1000

    more sinkhorn fas updates

--- function_library/graph_funs/fvs.py
+++ function_library/graph_funs/fvs.py
@@ -3,6 +3,7 @@ import random
 import functools
 
 import numpy as np
+import scipy.sparse
 import networkx as nx
 
 from .graph import copy_graph, get_loop_nodes, strongly_connected_components, adjacency_matrix
@@ -50,6 +51,45 @@ def sinkhorn_transform(A, N_iter=None, tol=1e-9, return_transform=False, copy=Tr
     else:
         return A
     
+def sinkhorn_transform2(A, N_iter=None, g=0.0, tol=1e-9, debug=False):
+    '''Iterative algorithm for sinkhorn balancing transformation with a
+    damping parameter g.
+
+    Taken from: https://www.cerfacs.fr/algor/reports/2006/TR_PA_06_42.pdf
+
+    I've optimized this with scipy sparse arrays and it's pretty fast.
+
+    The returned vectors can be used to transform the input matrix A into
+    a doubly stochastic matrix in the following way.
+    S = np.diag(r)@A@np.diag(c)
+    You can check that np.sum(S, axis=1) == np.sum(S, axis=0) == np.ones_like(r)
+
+    The diagonal of the doubly stochastic matrix is given by r*c.
+    '''
+    n = A.shape[0]
+    if N_iter is None:
+        N_iter = int(np.round(4*n*np.log(n)))
+    try:
+        # assume A is a scipy sparse matrix
+        At = A.T.tocsr() # csr is faster at matvec
+    except AttributeError:
+        # assume A is a numpy array
+        At = A.T
+    r = np.ones(n)
+    c = np.ones(n)
+    d = At@r + g*np.sum(r)
+    res = np.linalg.norm(c*d - 1, 1)
+    i = 0
+    while (res > tol) and (i < N_iter):
+        i += 1
+        c = 1/d
+        r = 1/(A@c + g*np.sum(c))
+        d = At@r + g*np.sum(r)
+        res = np.linalg.norm(c*d - 1, 1)
+    if debug:
+        print(i, res)
+    return r, c
+
 def mdfvs_max_deg(G, fvs=None, copy=True):
     '''A reasonable hueristic but not as good as some of the 
     more sophisticated ones.
@@ -200,34 +240,77 @@ def mdfvs_sinkhorn(G, Nsinkhorn=None, fvs=None, copy=True, debug=False):
     G.remove_node(v)
     return mdfvs_sinkhorn(G, fvs=fvs, copy=False)
 
-def sinkhorn_rank(G, tol=1e-9, maxiter=None):
-    A = nx.to_numpy_array(G)
-    A = A + np.eye(len(A))
-    H = sinkhorn_transform(A, tol=tol, N_iter=maxiter)
-    H_diag = np.diag(H)
+def sinkhorn_rank(G, tol, g=0, s=1, maxiter=None, sparse=False):
+    N = len(G.nodes)
+    if sparse:
+        A = nx.to_scipy_sparse_array(G)
+        A = A + scipy.sparse.diags(np.ones(N)*s)
+    else:
+        A = nx.to_numpy_array(G)
+        A = A + np.eye(N)*s
+    r, c = sinkhorn_transform2(A, tol=tol, g=g, N_iter=maxiter)
+    H_diag = r*c
     out_dict = {n:x for n,x in zip(G.nodes, H_diag)}
     return out_dict
 
-def sinkhorn_fas(G, tol=1e-9, maxiter=None, debug=False):
+def sinkhorn_fas(G, tol=1e-9, s=1, g=0, maxiter=None, inplace=False, debug=False, sparse=True, global_graph=False):
+    if not inplace:
+        G = G.copy()
+    fas = list()
+    sccs = True
+    jj = 0
+    while sccs:
+        jj += 1
+        if not global_graph:
+            sccs = [x for x in nx.strongly_connected_components(G) if len(x) > 1]
+            for scc in sccs:
+                # print(scc)
+                Lsc = nx.line_graph(G.subgraph(scc))
+
+                Lsc_rank = sinkhorn_rank(Lsc, tol=tol, g=g, s=s, maxiter=maxiter, sparse=sparse)
+                sc_edge = min(Lsc_rank, key=lambda x: Lsc_rank[x])
+                
+                # print(sc_edge)
+                fas.append(sc_edge)
+                G.remove_edge(*sc_edge)
+        else:
+            L = nx.line_graph(G)
+            L_rank = sinkhorn_rank(L, tol=tol, g=g, s=s, maxiter=maxiter, sparse=sparse)
+            sc_edge = min(L_rank, key=lambda x: L_rank[x])
+            fas.append(sc_edge)
+            G.remove_edge(*sc_edge)
+            sccs = any(len(x) > 1 for x in nx.strongly_connected_components(G))
+
+    if debug:
+        print(jj)
+    return fas, G
+
+def sinkhorn_fas_slow(G, tol=1e-9, maxiter=None, debug=False):
+    '''
+    This version operates on the whole graph instead of individual
+    scc subgraphs and hence can be much slower. From limited testing on
+    random unweighted SCC graphs this method produces different FAS elements 
+    than the faster variant but the size of the FAS is always the same.
+    '''
     G = G.copy()
     fas = list()
     sccs = True
     jj = 0
     while sccs:
         jj += 1
-        sccs = [x for x in nx.strongly_connected_components(G) if len(x) > 1]
         if debug:
             print(len(sccs))
-        for scc in sccs:
-            # print(scc)
-            lsc = nx.line_graph(G.subgraph(scc))
 
-            lsc_rank = sinkhorn_rank(lsc, tol=tol, maxiter=maxiter)
-            sc_edge = min(lsc_rank, key=lambda x: lsc_rank[x])
-            
-            # print(sc_edge)
-            fas.append(sc_edge)
-            G.remove_edge(*sc_edge)
+        # print(scc)
+        L = nx.line_graph(G)
+
+        L_rank = sinkhorn_rank(L, tol=tol, maxiter=maxiter)
+        sc_edge = min(L_rank, key=lambda x: L_rank[x])
+        
+        # print(sc_edge)
+        fas.append(sc_edge)
+        G.remove_edge(*sc_edge)
+        sccs = any(len(x) > 1 for x in nx.strongly_connected_components(G))
     if debug:
         print(jj)
     return fas, G
@@ -245,16 +328,17 @@ def pagerank(G, tol=1e-9, maxiter=None):
         ii += 1
         v_old = v
         v = A@v
-        diff = np.sum(np.linalg.norm(v_old - v, ord=1))
+        diff = np.linalg.norm(v_old - v, ord=1)
     out_dict = {n:x for n,x in zip(G.nodes, v)}
     return out_dict
 
-def pagerank_fas(G, tol=1e-9, maxiter=None, debug=0):
+def pagerank_fas(G, tol=1e-9, maxiter=None, inplace=False, debug=0):
     '''
     Based off of the 2022 paper by Geladaris et al: Computing a Feedback Arc Set Using PageRank 
     https://arxiv.org/abs/2208.09234
     '''
-    G = G.copy()
+    if not inplace:
+        G = G.copy()
     fas = list()
     sccs = True
     jj = 0
@@ -273,6 +357,38 @@ def pagerank_fas(G, tol=1e-9, maxiter=None, debug=0):
             # print(sc_edge)
             fas.append(sc_edge)
             G.remove_edge(*sc_edge)
+    if debug:
+        print(jj)
+    return fas, G
+
+def pagerank_fas_slow(G, tol=1e-9, maxiter=None, inplace=False, debug=0):
+    '''
+    This version operates on the whole graph instead of individual
+    scc subgraphs and hence can be much slower. From limited testing on
+    random unweighted SCC graphs this method produces much larger FAS than
+    the faster version. Almost twice as big in most cases. Probably something
+    about the pagerank hueristic doesn't generalize to the whole graph.
+    '''
+    if not inplace:
+        G = G.copy()
+    fas = list()
+    sccs = True
+    jj = 0
+    while sccs:
+        jj += 1
+        if debug:
+            print(len(sccs))
+
+        # print(scc)
+        L = nx.line_graph(G)
+
+        L_rank = pagerank(L, tol=tol, maxiter=maxiter)
+        sc_edge = max(L_rank, key=lambda x: L_rank[x])
+        
+        # print(sc_edge)
+        fas.append(sc_edge)
+        G.remove_edge(*sc_edge)
+        sccs = any(len(x) > 1 for x in nx.strongly_connected_components(G))
     if debug:
         print(jj)
     return fas, G
